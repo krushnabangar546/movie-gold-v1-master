3466a5c394ca102bf8d27217b0a0e209bf8a21fa
Krushna Bangar <71035359+krushnabangar546@users.noreply.github.com>
2024-04-06 11:51:52 +0530

    first commit

---
+++ src/api/setupProxy.js
@@ -0,0 +1,4 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function(app) {
+  app.use('/api/v1/movies', createProxyMiddleware({ target: 'http://localhost:8080', changeOrigin: true }));
+};
